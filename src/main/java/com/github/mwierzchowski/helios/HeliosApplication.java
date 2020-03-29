d2e11dae1f2271500c93726b76ff76c975f0df88
mwierzchowski <mwierzch@gmail.com>
2020-03-29 20:59:53 +0200

    Enable Spring Data JPA

--- src/main/java/com/github/mwierzchowski/helios/HeliosApplication.java
+++ src/main/java/com/github/mwierzchowski/helios/HeliosApplication.java
@@ -3,6 +3,7 @@ package com.github.mwierzchowski.helios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @EnableCaching
 @EnableScheduling
+@EnableJpaRepositories
 @EnableTransactionManagement
 @SpringBootApplication
 public class HeliosApplication {

61adbb694866f59edf47e76868dc3aee02f8c66c
mwierzchowski <mwierzch@gmail.com>
2020-04-05 19:43:01 +0200

    Add JPA annotations

--- src/main/java/com/github/mwierzchowski/helios/core/timers/Timer.java
+++ src/main/java/com/github/mwierzchowski/helios/core/timers/Timer.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Timer {
+    @Id
+    @GeneratedValue(generator = "timer_id_sequence")
     private Integer id;
+
     private String description;
 }

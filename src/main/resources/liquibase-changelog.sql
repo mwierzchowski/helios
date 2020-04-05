0044e2ae9168e36a9c156efb79d35d082dde4412
mwierzchowski <mwierzch@gmail.com>
2020-04-05 19:42:31 +0200

    Add TImer table

--- src/main/resources/liquibase-changelog.sql
+++ src/main/resources/liquibase-changelog.sql
@@ -1,2 +1,9 @@
 --liquibase formatted sql
 
+--changeset mwierzchowski:create-timer-table
+create sequence timer_id_sequence increment by 50;
+
+create table timer (
+   id int primary key,
+   description varchar(255) unique not null
+);

89db8153232bf212b79f2c835c3e159a045f1bee
mwierzchowski <mwierzch@gmail.com>
2020-04-13 15:07:39 +0200

    Simplify static location handling

--- src/main/java/com/github/mwierzchowski/helios/core/locations/StaticLocationProperties.java
+++ src/main/java/com/github/mwierzchowski/helios/core/locations/StaticLocationProperties.java
@@ -1,17 +1,31 @@
 package com.github.mwierzchowski.helios.core.locations;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+import static java.util.Optional.ofNullable;
 
 /**
  * Static location properties.
  * @author Marcin Wierzchowski
  */
 @Data
+@Slf4j
 @Component
 @ConfigurationProperties("helios.location")
-public class StaticLocationProperties {
+@ConditionalOnProperty(prefix = "helios.location", name = {"latitude", "longitude"})
+public class StaticLocationProperties implements LocationProvider {
+    /**
+     * Default city name to be used when city was not provided.
+     */
+    private String defaultCity = "unknown city";
+
     /**
      * City
      */
@@ -26,4 +40,19 @@ public class StaticLocationProperties {
      * Geographical longitude
      */
     private Double longitude;
+
+    @PostConstruct
+    public void reportLocation() {
+        log.info("Static location is {} (lat={}, lon={})", cityOrDefault(), latitude, longitude);
+    }
+
+    @Override
+    @Cacheable("static-location")
+    public Location locate() {
+        return new Location(cityOrDefault(), latitude, longitude);
+    }
+
+    private String cityOrDefault() {
+        return ofNullable(city).orElse(defaultCity);
+    }
 }

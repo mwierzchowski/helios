d8d12da4929a358239af9bd1f7570273cc00be0d
mwierzchowski <mwierzch@gmail.com>
2020-04-14 16:47:44 +0200

    Add more test data for timers

--- src/test/resources/data/timer-data.sql
+++ src/test/resources/data/timer-data.sql
@@ -1,3 +1,13 @@
 select nextval ('timer_id_sequence');
-insert into timer (id, description, created, updated, version) values (1, 'test timer 1', '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
-insert into timer (id, description, created, updated, version) values (2, 'test timer 2', '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
+insert into timer (id, description, created, updated, version)
+    values (1, 'test timer 1', '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
+insert into timer (id, description, created, updated, version)
+    values (2, 'test timer 2', '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
+
+select nextval ('timerschedule_id_sequence');
+insert into timer_schedule (id, timer_id, time, days, enabled, created, updated, version)
+    values (1, 1, '06:30:00', '1,2,3,4,5', true, '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
+insert into timer_schedule (id, timer_id, time, days, enabled, created, updated, version)
+    values (2, 1, '08:00:00', '6,7', true, '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
+insert into timer_schedule (id, timer_id, time, days, enabled, created, updated, version)
+    values (3, 2, '10:00:00', '6', false, '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);

eef1516af04929425593d23475acb8a188a34b3a
mwierzchowski <mwierzch@gmail.com>
2020-04-19 21:04:29 +0200

    Fix test data

--- src/test/resources/data/timer-data.sql
+++ src/test/resources/data/timer-data.sql
@@ -1,10 +1,10 @@
-select nextval ('timer_id_sequence');
+select nextval ('timer_id_seq');
 insert into timer (id, description, created, updated, version)
     values (1, 'test timer 1', '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
 insert into timer (id, description, created, updated, version)
     values (2, 'test timer 2', '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
 
-select nextval ('timerschedule_id_sequence');
+select nextval ('timer_schedule_id_seq');
 insert into timer_schedule (id, timer_id, time, days, created, updated, version)
     values (1, 1, '06:30:00', '1,2,3,4,5', '2020-04-05T22:33:37.829+0200', '2020-04-05T22:33:37.829+0200', 1);
 insert into timer_schedule (id, timer_id, time, days, created, updated, version)

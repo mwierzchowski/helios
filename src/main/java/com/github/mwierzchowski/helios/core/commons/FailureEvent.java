f6e15959f904b9849f2b71d20f3c5c002bd0440f
mwierzchowski <mwierzch@gmail.com>
2020-04-26 20:59:25 +0200

    Add FailureEvent

---
+++ src/main/java/com/github/mwierzchowski/helios/core/commons/FailureEvent.java
@@ -0,0 +1,27 @@
+package com.github.mwierzchowski.helios.core.commons;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+/**
+ * Event published in case of failures
+ * @author Marcin Wierzchowski
+ */
+@Data
+public class FailureEvent implements HeliosEvent {
+    /**
+     * Timestamp of event
+     */
+    private final Instant timestamp = Instant.now();
+
+    /**
+     * Component that send this event
+     */
+    private final Class<?> source;
+
+    /**
+     * Throwable of the failure
+     */
+    private final Throwable throwable;
+}

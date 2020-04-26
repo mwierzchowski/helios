f272d3a60f841790f150ca3a06e34ed256c6c6ae
mwierzchowski <mwierzch@gmail.com>
2020-04-27 00:15:52 +0200

    Add comparator to TimestampedHeliosEvent

--- src/main/java/com/github/mwierzchowski/helios/core/commons/TimestampedHeliosEvent.java
+++ src/main/java/com/github/mwierzchowski/helios/core/commons/TimestampedHeliosEvent.java
@@ -6,6 +6,13 @@ import java.time.Instant;
  * Interface for time-stamped application events.
  * @author Marcin Wierzchowski
  */
-public interface TimestampedHeliosEvent extends HeliosEvent {
+public interface TimestampedHeliosEvent extends HeliosEvent, Comparable<TimestampedHeliosEvent> {
     Instant getTimestamp();
+
+    @Override
+    default int compareTo(TimestampedHeliosEvent other) {
+        var timestamp1 = this.getTimestamp().toEpochMilli();
+        var timestamp2 = other.getTimestamp().toEpochMilli();
+        return (int) (timestamp1 - timestamp2);
+    }
 }

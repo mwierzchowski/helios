bfb96ec7bfd8a3649ee54a0b7c26b5459c7437cf
mwierzchowski <mwierzch@gmail.com>
2020-05-10 10:36:48 +0200

    Cleanup

--- src/testE2E/groovy/com/github/mwierzchowski/helios/actuator/HealthE2ETest.groovy
+++ src/testE2E/groovy/com/github/mwierzchowski/helios/actuator/HealthE2ETest.groovy
@@ -3,20 +3,21 @@ package com.github.mwierzchowski.helios.actuator
 import org.junit.Test
 
 import static io.restassured.RestAssured.when
-import static org.hamcrest.Matchers.*
+import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.notNullValue
 
 class HealthE2ETest {
     def healthUrl = "/actuator/health"
 
     @Test
-    void "General health status should be UP"() {
+    void "Should return general health status UP"() {
         when().get(healthUrl).then()
                 .statusCode(200)
-                .body("status", equalTo("UP"))
+                .body("status", is("UP"))
     }
 
     @Test
-    void "OWM health status should be UP"() {
+    void "Should return OWM health status UP"() {
         when().get(healthUrl).then()
                 .body("components.owm.status", is("UP"))
                 .body("components.owm.details.failureRate", is(0.0f))
@@ -24,7 +25,7 @@ class HealthE2ETest {
     }
 
     @Test
-    void "Sun API health status should be UP"() {
+    void "Should return Sun API health status UP"() {
         when().get(healthUrl).then()
                 .body("components.sunApi.status", is("UP"))
                 .body("components.sunApi.details.failureRate", is(0.0f))
@@ -32,14 +33,14 @@ class HealthE2ETest {
     }
 
     @Test
-    void "DB health status should be UP"() {
+    void "Should return DB health status UP"() {
         when().get(healthUrl).then()
                 .body("components.db.status", is("UP"))
                 .body("components.db.details.database", is("PostgreSQL"))
     }
 
     @Test
-    void "Mail health status should be UP"() {
+    void "Should return mail health status UP"() {
         when().get(healthUrl).then()
                 .body("components.mail.status", is("UP"))
     }

e71b40efc08d7acd82575a0116b6c25c2bd54c81
mwierzchowski <mwierzch@gmail.com>
2020-05-10 10:27:24 +0200

    More Health checks

--- src/testE2E/groovy/com/github/mwierzchowski/helios/actuator/HealthE2ETest.groovy
+++ src/testE2E/groovy/com/github/mwierzchowski/helios/actuator/HealthE2ETest.groovy
@@ -3,33 +3,44 @@ package com.github.mwierzchowski.helios.actuator
 import org.junit.Test
 
 import static io.restassured.RestAssured.when
-import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.*
 
 class HealthE2ETest {
     def healthUrl = "/actuator/health"
 
     @Test
     void "General health status should be UP"() {
-        when().get(healthUrl)
-        .then().statusCode(200)
+        when().get(healthUrl).then()
+                .statusCode(200)
                 .body("status", equalTo("UP"))
     }
 
     @Test
     void "OWM health status should be UP"() {
-        when().get(healthUrl)
-        .then().body("components.owm.status", equalTo("UP"))
+        when().get(healthUrl).then()
+                .body("components.owm.status", is("UP"))
+                .body("components.owm.details.failureRate", is(0.0f))
+                .body("components.owm.details.lastSuccess.response.main.temp", notNullValue())
     }
 
     @Test
     void "Sun API health status should be UP"() {
-        when().get(healthUrl)
-        .then().body("components.sunApi.status", equalTo("UP"))
+        when().get(healthUrl).then()
+                .body("components.sunApi.status", is("UP"))
+                .body("components.sunApi.details.failureRate", is(0.0f))
+                .body("components.sunApi.details.lastSuccess.response.status", is("OK"))
+    }
+
+    @Test
+    void "DB health status should be UP"() {
+        when().get(healthUrl).then()
+                .body("components.db.status", is("UP"))
+                .body("components.db.details.database", is("PostgreSQL"))
     }
 
     @Test
     void "Mail health status should be UP"() {
-        when().get(healthUrl)
-                .then().body("components.mail.status", equalTo("UP"))
+        when().get(healthUrl).then()
+                .body("components.mail.status", is("UP"))
     }
 }

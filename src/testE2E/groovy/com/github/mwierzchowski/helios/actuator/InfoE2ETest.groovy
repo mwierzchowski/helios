43e62da91c59e9ee62e13f75a392e78d4d1f3459
Marcin Wierzchowski <mwierzch@gmail.com>
2020-05-10 10:48:14 +0200

    Feature/functional tests (#22)
    Add docker-compose, test environment automation and E2E tests

---
+++ src/testE2E/groovy/com/github/mwierzchowski/helios/actuator/InfoE2ETest.groovy
@@ -0,0 +1,38 @@
+package com.github.mwierzchowski.helios.actuator
+
+import org.junit.Test
+
+import static io.restassured.RestAssured.when
+import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.notNullValue
+
+class InfoE2ETest {
+    def infoUrl = "/actuator/info"
+
+    @Test
+    void "Should return application info"() {
+        when().get(infoUrl).then()
+                .statusCode(200)
+                .body("app.name", notNullValue())
+    }
+
+    @Test
+    void "Should return location info"() {
+        when().get(infoUrl).then()
+                .body("location.latitude", notNullValue())
+                .body("location.longitude", notNullValue())
+    }
+
+    @Test
+    void "Should return sun ephemeris info"() {
+        when().get(infoUrl).then()
+                .body("sunEphemeris.approximated", is(false))
+    }
+
+    @Test
+    void "Should return weather info"() {
+        when().get(infoUrl).then()
+                .body("weather.provided", is(true))
+                .body("weather.temperature.value", notNullValue())
+    }
+}

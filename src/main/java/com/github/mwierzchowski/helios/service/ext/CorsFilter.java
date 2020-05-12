615a2093a842c502a752982ed9fd31eee8f70029
mwierzchowski <mwierzch@gmail.com>
2020-05-12 23:08:01 +0200

    update javadoc

--- src/main/java/com/github/mwierzchowski/helios/service/ext/CorsFilter.java
+++ src/main/java/com/github/mwierzchowski/helios/service/ext/CorsFilter.java
@@ -7,10 +7,18 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
+/**
+ * Filter provides CORS enabling HTTP headers.
+ * @author Marcin Wierzchowski
+ */
 @Provider
 @Component
 public class CorsFilter implements ContainerResponseFilter {
-
+    /**
+     * Main filter method
+     * @param requestContext request
+     * @param responseContext response
+     */
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");

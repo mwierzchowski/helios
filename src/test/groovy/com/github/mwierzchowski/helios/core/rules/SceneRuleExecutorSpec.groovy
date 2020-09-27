93bbd486b0830dc5592d095c9aab75af898ead07
Marcin Wierzchowski <mwierzch@gmail.com>
2020-09-27 19:42:12 +0200

    Fix/application fails cicd (#66)
    * Exten wait time for application to start
    
    * Disable mail notifications on E2E test
    
    * Disable mail notifications on E2E test
    
    * Disable mail notifications on E2E test
    
    * Add some tests to improve coverage
    
    * Add some tests to improve coverage

--- src/test/groovy/com/github/mwierzchowski/helios/core/rules/SceneRuleExecutorSpec.groovy
+++ src/test/groovy/com/github/mwierzchowski/helios/core/rules/SceneRuleExecutorSpec.groovy
@@ -2,16 +2,30 @@ package com.github.mwierzchowski.helios.core.rules
 
 import com.github.mwierzchowski.helios.core.timers.Timer
 import com.github.mwierzchowski.helios.core.timers.TimerAlertEvent
+import com.github.mwierzchowski.helios.core.weather.Weather
+import com.github.mwierzchowski.helios.core.weather.WeatherStaleEvent
 import spock.lang.Specification
 
 class SceneRuleExecutorSpec extends Specification {
     def properties = new SceneRuleProperties()
     def executor = new SceneRuleExecutor(properties)
 
-    def "Dummy test to be replaced later"() {
+    // TODO this specification is rather dummy
+    def "Should update facts on supported events"() {
         given:
         def timer = new Timer()
         def event = new TimerAlertEvent(timer)
+        when:
+        executor.executeRulesFor(event)
+        then:
+        noExceptionThrown()
+    }
+
+    // TODO this specification is rather dummy
+    def "Should reset facts on not supported events"() {
+        given:
+        def weather = new Weather()
+        def event = new WeatherStaleEvent(weather)
         when:
         executor.executeRulesFor(event)
         then:

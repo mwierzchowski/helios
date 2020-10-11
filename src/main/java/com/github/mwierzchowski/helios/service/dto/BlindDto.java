6501fcd5c5a83e1d24012d67d58bacb824aaa479
Marcin Wierzchowski <mwierzch@gmail.com>
2020-10-11 21:26:45 +0200

    Add move and stop operations

---
+++ src/main/java/com/github/mwierzchowski/helios/service/dto/BlindDto.java
@@ -0,0 +1,11 @@
+package com.github.mwierzchowski.helios.service.dto;
+
+import lombok.Data;
+
+@Data
+public class BlindDto {
+    private Integer id;
+    private String description;
+    private String status;
+    private Integer position;
+}
